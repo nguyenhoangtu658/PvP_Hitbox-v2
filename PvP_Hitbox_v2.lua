f142206a74f28deddc7236aabdc2179edab08c03
nguyenhoangtu658 <ca0360056@gmail.com>
2025-04-16 11:56:44 +0700

    Add files via upload

---
+++ PvP_Hitbox_v2.lua
@@ -0,0 +1,15 @@
+spawn(function()
+    while wait(1) do
+        for _, player in pairs(game.Players:GetPlayers()) do
+            if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                pcall(function()
+                    local hrp = player.Character.HumanoidRootPart
+                    hrp.Size = Vector3.new(500, 500, 500)
+                    hrp.Transparency = 0.7
+                    hrp.Material = Enum.Material.ForceField
+                    hrp.CanCollide = false
+                end)
+            end
+        end
+    end
+end)
