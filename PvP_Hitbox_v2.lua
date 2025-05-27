16eb35df8a2b2faffbc82e9e12e193ef1b91efbb
nguyenhoangtu658 <ca0360056@gmail.com>
2025-05-27 13:10:38 +0700

    Update PvP_Hitbox_v2.lua

--- PvP_Hitbox_v2.lua
+++ PvP_Hitbox_v2.lua
@@ -1,11 +1,11 @@
 spawn(function()
-    while wait(1) do
+    while wait(0.5) do
         for _, player in pairs(game.Players:GetPlayers()) do
             if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
                 pcall(function()
                     local hrp = player.Character.HumanoidRootPart
-                    hrp.Size = Vector3.new(500, 500, 500)
-                    hrp.Transparency = 0.7
+                    hrp.Size = Vector3.new(300, 300, 300)
+                    hrp.Transparency = 1
                     hrp.Material = Enum.Material.ForceField
                     hrp.CanCollide = false
                 end)
